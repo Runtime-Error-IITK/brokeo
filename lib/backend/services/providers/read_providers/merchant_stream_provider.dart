bdf0e0dbaf2fbfb90fda2e4640fd9dd6def49d61
Aujasvit Datta <80042330+aujasvit@users.noreply.github.com>
2025-04-02 12:10:02 +0530

    added unsaved files

--- lib/backend/services/providers/read_providers/merchant_stream_provider.dart
+++ lib/backend/services/providers/read_providers/merchant_stream_provider.dart
@@ -1,5 +1,5 @@
 import 'package:brokeo/backend/models/merchant.dart';
-import 'package:brokeo/backend/services/providers2/read_providers/user_id_provider.dart'
+import 'package:brokeo/backend/services/providers/read_providers/user_id_provider.dart'
     show userIdProvider;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
